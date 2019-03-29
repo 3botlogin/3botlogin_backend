8b817cd0165acf6dc1637286148bda2b397a6f2f
Ivan Coene <ivan@ivancoene.be>
2019-03-29 09:57:27 +0100

    Verifing signature

--- __main__.py
+++ __main__.py
@@ -2,7 +2,13 @@
 from flask import Flask, Response, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-import time
+import datetime
+from datetime import timedelta  
+import nacl.signing
+import nacl.encoding
+import binascii
+import struct
+import base64
 
 app = Flask(__name__)
 sio = SocketIO(app)
@@ -12,7 +18,7 @@ users = []
 login_attempts = []
 
 
-def findUser(query, search_for="double_name"):
+def find_user(query, search_for="double_name"):
     print('Search for', query)
     for u in users:
         if u.get(search_for) == query:
@@ -40,11 +46,9 @@ def identification_handler(data):
     print('')
     print('< identify', data)
     sid = request.sid
-    print(data.get('doubleName'))
-    print (sid)
-    if any(data.get('doubleName') in user.get('double_name') for user in users):
+    user = find_user(data.get('doubleName'))
+    if user:
         print('> sending nameknown')
-        user = findUser(data.get('doubleName'))
         print('found user', user)
         user['sid'] = sid
         emit('nameknown')
@@ -60,7 +64,7 @@ def identification_handler(data):
 def registration_handler(data):
     print('')
     print('< register', data)
-    user = findUser(data.get('doubleName'))
+    user = find_user(data.get('doubleName'))
     user['public_key'] = data.get('publicKey')
     user['email'] = data.get('email')
     print('')
@@ -73,7 +77,7 @@ def login_handler(data):
     login_attempts.append({
         'double_name': data.get('doubleName'),
         'state': data.get('state'),
-        'timestamp': time.time(),
+        'timestamp': datetime.datetime.now(),
         'scanned': False
     })
     print(find_loggin_attempt(data.get('state')))
@@ -93,8 +97,9 @@ def flag_handler():
         print('user', user)
         user['scanned'] = True
         sio.emit('scannedFlag', room=user.get('sid'))
-    response = Response("bla")
-    return response
+        return Response("Ok")
+    else: 
+        return Response('User not found', status=404)
 
 
 @app.route('/api/sign', methods=['POST'])
@@ -105,16 +110,37 @@ def sign_handler():
     user = find_loggin_attempt(body.get('hash'))
     if user:
         print('user', user)
-        user['singedStateHash'] = body.get('signedHash')
+        user['singed_statehash'] = body.get('signedHash')
         sio.emit('signed', body.get('signedHash'), room=user.get('sio'))
-    return Response("K")
-
+    return Response("Ok")
 
 @app.route('/api/verify', methods=['POST'])
 def verify_handler():
     print('')
     body = request.get_json()
     print('< verify', body)
-    return Response("K")
+    user = find_user(body.get('username'))
+    login_attempt = find_loggin_attempt(body.get('hash'))
+    if user and login_attempt:
+        requested_datetime = login_attempt.get('timestamp')
+        max_datetime = requested_datetime + timedelta(minutes=10)
+        if requested_datetime < max_datetime:
+            public_key = base64.b64decode(user.get('public_key'))
+            signed_hash = base64.b64decode(login_attempt.get('singed_statehash'))
+            original_hash = login_attempt.get('state')
+            try:
+                bytes_signed_hash = bytes(signed_hash)
+                bytes_original_hash = bytes(original_hash, encoding='utf8')
+                verify_key = nacl.signing.VerifyKey(public_key.hex(), encoder=nacl.encoding.HexEncoder)
+                verify_key.verify(bytes_original_hash, bytes_signed_hash)
+                return Response("Ok")
+            except:
+                return Response("Sinature invalid", status=400)
+        else:
+            return Response("You are too late", status=400)
 
-app.run(host='0.0.0.0', port= 5000)
+    else:
+        return Response("Oops.. user or loggin attempt not found", status=404)
+
+
+app.run(host='0.0.0.0', port=5000)
