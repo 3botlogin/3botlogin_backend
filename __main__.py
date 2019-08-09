0f1f33f2f8456155160bf4bc0e07352adaf04742
Mathias <mathias.de.weerdt@gmail.com>
2019-08-09 09:18:09 +0200

    OpenKyc

--- __main__.py
+++ __main__.py
@@ -34,10 +34,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
 handler = logging.StreamHandler()
-formatter = logging.Formatter("[%(asctime)s][%(filename)s:%(lineno)s - %(funcName)s()]: %(message)s", "%Y-%m-%d %H:%M:%S")
+formatter = logging.Formatter(
+    "[%(asctime)s][%(filename)s:%(lineno)s - %(funcName)s()]: %(message)s", "%Y-%m-%d %H:%M:%S")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
+
 
 @sio.on('connect')
 def connect_handler():
@@ -49,13 +51,14 @@ def checkname_handler(data):
     logger.debug("Checking name %s", data)
     sid = request.sid
     user = db.getUserByName(conn, data.get('doubleName').lower())
-    
+
     if user:
         logger.debug("user %s", user[0])
         emit('nameknown')
     else:
         logger.debug("user %s was not found", data.get('doubleName').lower())
         emit('namenotknown')
+
 
 @sio.on('cancel')
 def cancel_handler(data):
@@ -90,7 +93,7 @@ def login_handler(data):
     if data.get('firstTime') == False and data.get('mobile') == False:
         user = db.getUserByName(conn, data.get('doubleName').lower())
         push_service.notify_single_device(registration_id=user[4], message_title='Finish login',
-                                          message_body='Tap to finish login', data_message=data, click_action='FLUTTER_NOTIFICATION_CLICK', tag='testLogin', collapse_key='testLogin' )
+                                          message_body='Tap to finish login', data_message=data, click_action='FLUTTER_NOTIFICATION_CLICK', tag='testLogin', collapse_key='testLogin')
 
     insert_auth_sql = "INSERT INTO auth (double_name,state_hash,timestamp,scanned,data) VALUES (?,?,?,?,?);"
     db.insert_auth(conn, insert_auth_sql, data.get('doubleName').lower(
@@ -102,14 +105,12 @@ def login_handler(data):
 def resend_handler(data):
     logger.debug("Resend %s", data)
 
-
     db.delete_auth_for_user(conn, data.get('doubleName').lower())
 
     insert_auth_sql = "INSERT INTO auth (double_name,state_hash,timestamp,scanned,data) VALUES (?,?,?,?,?);"
 
     db.insert_auth(conn, insert_auth_sql, data.get('doubleName').lower(
     ), data.get('state'), datetime.now(), 0, json.dumps(data))
-
 
     user = db.getUserByName(conn, data.get('doubleName').lower())
     data['type'] = 'login'
@@ -177,7 +178,7 @@ def flag_handler():
 
                 update_sql = "UPDATE users SET device_id =?  WHERE double_name=?;"
                 db.update_user(conn, update_sql,
-                                verified_device_id, login_attempt[0])
+                               verified_device_id, login_attempt[0])
 
                 sio.emit('scannedFlag', {'scanned': True}, room=user[1])
             return Response("Ok")
@@ -225,14 +226,17 @@ def get_attempts_handler(doublename):
                     current_timestamp = time.time() * 1000
                     readable_current_timestamp = datetime.fromtimestamp(
                         int(current_timestamp / 1000))
-                    difference = (int(timestamp) - int(current_timestamp)) / 1000
+                    difference = (int(timestamp) -
+                                  int(current_timestamp)) / 1000
                     if difference < 30:
                         logger.debug("Verification succeeded.")
-                        login_attempt = db.getAuthByDoubleName(conn, doublename)
+                        login_attempt = db.getAuthByDoubleName(
+                            conn, doublename)
                         if (login_attempt is not None):
                             logger.debug("Login attempt %s", login_attempt)
                             response = app.response_class(
-                                response=json.dumps(json.loads(login_attempt[5])),
+                                response=json.dumps(
+                                    json.loads(login_attempt[5])),
                                 mimetype='application/json'
                             )
                             return response
@@ -240,21 +244,22 @@ def get_attempts_handler(doublename):
                             logger.debug("No login attempts found")
                             return Response("No login attempts found", status=204)
                     else:
-                        logger.debug("Signed timestamp inside the header has expired")
+                        logger.debug(
+                            "Signed timestamp inside the header has expired")
                         # return Response("Signed timestamp inside the header has expired", status=400)
                 else:
                     logger.debug("Intention was not correct!")
             else:
-                logger.debug("Signed timestamp inside the header could not be verified")
+                logger.debug(
+                    "Signed timestamp inside the header could not be verified")
                 # return Response("Signed timestamp inside the header could not be verified", status=400)
         else:
             logger.debug("Header was not present")
             # return Response("Header was not present", status=400)
     except Exception as e:
-        logger.debug("Something went wrong while trying to verify the header %e", e)
+        logger.debug(
+            "Something went wrong while trying to verify the header %e", e)
         # return Response("Something went wrong while trying to verify the header", status=400)
-
-    
 
 
 @app.route('/api/verify', methods=['POST'])
@@ -263,7 +268,7 @@ def verify_handler():
     logger.debug("Verify %s", body)
     user = db.getUserByName(conn, body.get('username'))
     login_attempt = db.getAuthByStateHash(conn, body.get('hash'))
-    try: 
+    try:
         if user and login_attempt:
             requested_datetime = datetime.strptime(
                 login_attempt[2], '%Y-%m-%d %H:%M:%S.%f')
@@ -290,7 +295,6 @@ def verify_handler():
         logger.log("Something went wrong while trying to verify the header %e", e)
 
 
-
 @app.route('/api/users/<doublename>/deviceid', methods=['PUT'])
 def update_device_id(doublename):
     body = request.get_json()
@@ -308,7 +312,8 @@ def update_device_id(doublename):
                 db.update_deviceid(conn, device_id, doublename)
                 return device_id
             else:
-                logger.debug("Signed timestamp inside the header could not be verified")
+                logger.debug(
+                    "Signed timestamp inside the header could not be verified")
         else:
             logger.debug("Header was not present")
     except:
@@ -334,7 +339,8 @@ def remove_device_id(doublename):
                     current_timestamp = time.time() * 1000
                     readable_current_timestamp = datetime.fromtimestamp(
                         int(current_timestamp / 1000))
-                    difference = (int(timestamp) - int(current_timestamp)) / 1000
+                    difference = (int(timestamp) -
+                                  int(current_timestamp)) / 1000
                     if difference < 30:
                         db.update_deviceid(conn, "", doublename)
                         device_id = db.get_deviceid(conn, doublename)[0]
@@ -349,7 +355,8 @@ def remove_device_id(doublename):
                         logger.debug("Timestamp was expired")
                         return Response("Request took to long", status=418)
             else:
-                logger.debug("Signed timestamp inside the header could not be verified")
+                logger.debug(
+                    "Signed timestamp inside the header could not be verified")
                 return Response("something went wrong", status=404)
         else:
             logger.debug("Header was not present")
@@ -357,6 +364,7 @@ def remove_device_id(doublename):
     except:
         logger.debug("Something went wrong while trying to verify the header")
         return Response("something went wrong", status=400)
+
 
 @app.route('/api/users/<doublename>', methods=['GET'])
 def get_user_handler(doublename):
@@ -378,18 +386,22 @@ def get_user_handler(doublename):
         logger.debug("User not found")
         return Response('User not found', status=404)
 
+
 @app.route('/api/users/<doublename>/cancel', methods=['POST'])
 def cancel_login_attempt(doublename):
     user = db.getUserByName(conn, doublename.lower())
     db.delete_auth_for_user(conn, doublename.lower())
-    
+
     sio.emit('cancelLogin', {'scanned': True}, room=user[1])
     return Response('Canceled by User')
+
 
 @app.route('/api/users/<doublename>/emailverified', methods=['post'])
 def set_email_verified_handler(doublename):
     logger.debug("Verified email from user %s", doublename.lower())
     user = db.getUserByName(conn, doublename.lower())
+    logger.debug(user)
+    logger.debug(user[4])
     push_service.notify_single_device(registration_id=user[4], message_title='Email verified', message_body='Thanks for verifying your email', data_message={
                                       'type': 'email_verification'}, click_action='EMAIL_VERIFIED')
     return Response('Ok')
@@ -416,11 +428,14 @@ def save_derived_public_key():
                     current_timestamp = time.time() * 1000
                     readable_current_timestamp = datetime.fromtimestamp(
                         int(current_timestamp / 1000))
-                    difference = (int(timestamp) - int(current_timestamp)) / 1000
+                    difference = (int(timestamp) -
+                                  int(current_timestamp)) / 1000
                     if difference < 30:
-                        #here code
-                        derived_public_key = verify_signed_data(double_name, body.get('signedDerivedPublicKey')).decode(encoding='utf-8')
-                        app_id = verify_signed_data(double_name, body.get('signedAppId')).decode(encoding='utf-8')
+                        # here code
+                        derived_public_key = verify_signed_data(double_name, body.get(
+                            'signedDerivedPublicKey')).decode(encoding='utf-8')
+                        app_id = verify_signed_data(double_name, body.get(
+                            'signedAppId')).decode(encoding='utf-8')
 
                         if double_name and derived_public_key and app_id:
                             logger.debug("Signed data has been verified")
@@ -434,19 +449,21 @@ def save_derived_public_key():
                             return Response('', status=200)
                         else:
                             logger.debug("Signed data is not verified")
-                    
+
                         return Response("something went wrong", status=400)
                     else:
                         logger.debug("Timestamp was expired")
                         return Response("Request took to long", status=418)
             else:
-                logger.debug("Signed timestamp inside the header could not be verified")
+                logger.debug(
+                    "Signed timestamp inside the header could not be verified")
                 return Response("something went wrong", status=404)
         else:
             logger.debug("Header was not present")
             return Response("Header was not present", status=400)
     except Exception as e:
-        logger.debug("Something went wrong while trying to verify the header %s", e)
+        logger.debug(
+            "Something went wrong while trying to verify the header %s", e)
         return Response("something went wrong", status=400)
 
 
@@ -456,24 +473,24 @@ def min_version_handler():
 
 
 def verify_signed_data(double_name, data):
-    print('/n### --- data verification --- ###')
-    print("Verifying data: ", data)
+    # print('/n### --- data verification --- ###')
+    # print("Verifying data: ", data)
 
     decoded_data = base64.b64decode(data)
-    print("Decoding data: ", decoded_data)
+    # print("Decoding data: ", decoded_data)
 
     bytes_data = bytes(decoded_data)
 
     public_key = base64.b64decode(db.getUserByName(conn, double_name)[3])
-    print('Retrieving public key from: ', double_name)
+    # print('Retrieving public key from: ', double_name)
 
     verify_key = nacl.signing.VerifyKey(
         public_key.hex(), encoder=nacl.encoding.HexEncoder)
-    print('verify_key: ', verify_key)
+    # print('verify_key: ', verify_key)
 
     verified_signed_data = verify_key.verify(bytes_data)
-    print('verified_signed_data: ', verified_signed_data)
-    print('### --- END data verification --- ###/n')
+    # print('verified_signed_data: ', verified_signed_data)
+    # print('### --- END data verification --- ###/n')
 
     return verified_signed_data
 
