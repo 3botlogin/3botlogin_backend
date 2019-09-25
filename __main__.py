99845f554389b8af3e465bb9f63bcc62ab80f48d
coeneivan <ivan@ivancoene.be>
2019-09-25 15:44:36 +0200

    Hiding apps

--- __main__.py
+++ __main__.py
@@ -506,7 +506,7 @@ def save_derived_public_key():
 
 @app.route('/api/showapps', methods=['get'])
 def show_apps_handler():
-    return Response('True')
+    return Response('False')
 
 
 @app.route('/api/minversion', methods=['get'])
